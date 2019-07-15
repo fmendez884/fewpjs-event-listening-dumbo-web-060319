81f3a654855f9c67fcace205e8851ca388cf57e6
Francis Mendez <fmendez884@gmail.com>
2019-07-15 18:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var input = document.getElementById('input')
 
 function addingEventListener() {
+  const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
 }
